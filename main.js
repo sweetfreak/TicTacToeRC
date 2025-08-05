f9f6315ef6d2aa4bbb48bb666892c773d88134d8
jesse sheehan <jessetsheehan@gmail.com>
2025-08-05 09:50:26 -0400

    Game works except for "Win" state

--- main.js
+++ main.js
@@ -2,7 +2,20 @@ let xTurn = true
 const headerEl = document.getElementById('header')
 const boardEl = document.getElementById('board')
 const startButton = document.createElement('button')
-let turnsUsed = []
+let xsUsed = []
+let osUsed = []
+let win = false
+
+ const winners = [
+        [0, 1, 2],
+        [0, 4, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 4, 6],
+        [2, 5, 8],
+        [3, 4, 5],
+        [6, 7, 8] 
+    ]
 
 startButton.textContent ='Start Game'
 headerEl.appendChild(startButton)
@@ -13,8 +26,7 @@ startButton.addEventListener('click', startGame)
 function startGame() {
     for (let i = 0; i<9; i++){
         const buttonEl = document.createElement('button')
-        buttonEl.id = 'square'
-        let spaceFree = true
+        buttonEl.classList.add('square')
         boardEl.appendChild(buttonEl)
         buttonEl.dataset.index = i
         //console.log(buttonEl)
@@ -28,75 +40,98 @@ function startGame() {
 }
 
 function playerTurn(clickedButton) {
-    spaceFree = false
+    
     clickedButton.id = "spaceUsed"
-    turnsUsed.push(clickedButton.dataset.index)
-    console.log(turnsUsed)
+    clickedButton.disabled = true
+
+    //add to turns used array
+   // turnsUsed.push(clickedButton.dataset.index)
+    //console.log(turnsUsed)
 
     if (xTurn == true) {
         clickedButton.textContent = 'x'
-        clickedButton.disabled = true
-        checkForWin()
-        
+        xsUsed.push(clickedButton.dataset.index)
+        console.log(xsUsed)
+        win = isWinningCombo(xsUsed)
+
     } else {
         clickedButton.textContent = 'o'
-        clickedButton.disabled = true
-        checkForWin()
+        osUsed.push(clickedButton.dataset.index)
+        console.log(osUsed)
+        console.log(isWinningCombo(osUsed))
+        win = isWinningCombo(osUsed)
+    }
+
+
+    if (win) {
+        gameWon()
+    } else if ((xsUsed.length + osUsed.length) == 9) {
+        resetStartButton()
+    } else {
+     xTurn = !xTurn
     }
 }
+//SOMETHING WRONG WITH THIS
+function isWinningCombo(playerMoves) {
+  return winners.some(combo =>
+    combo.every(index => playerMoves.includes(index))
+  );
+}
 
-function checkForWin() {
-    let win = false
-    //array of arrays with winning combos.
-    const winners = [
-        [0, 1, 2],
-        [0, 4, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 4, 6],
-        [2, 5, 8],
-        [3, 4, 5],
-        [6, 7, 8] 
-    ]
-
-const usedSquaresArr = document.querySelectorAll('spaceUsed')
-
-
-
-
-
-    //THIS ISNT CHECKING THE DATA-INDEX, it's just looking at the numbers above?
-    //need to get the data indices, assign them, and then see if the textContent is the same?
-
-    winners.forEach(function(array) {
-
+// function checkForWin() {
     
-        //if (squaresArr[array[0]] == squaresArr[array[1]] && squaresArr[array[1]] == squaresArr[array[2]]) {
-        if (array[0] == array[1] && array[1] == array[2]) {
-            win = true
-            console.log('WINNER!!!')
-        } else {
-            console.log('no winner')
-        }
-    })
+//     //const usedSquaresArr = document.querySelectorAll('spaceUsed')
 
+//     //THIS ISNT CHECKING THE DATA-INDEX, it's just looking at the numbers above?
+//     //need to get the data indices, assign them, and then see if the textContent is the same?
 
-    
-    if (win == true) {
+//     // winners.forEach(function(array) {
+//     //     //if (squaresArr[array[0]] == squaresArr[array[1]] && squaresArr[array[1]] == squaresArr[array[2]]) {
+//     //    // if (array[0] == array[1] && array[1] == array[2]) {
+//     //         win = true
+//     //         console.log('WINNER!!!')
+//     //     } else if (turnsUsed.length == 9) {
+//     //         resetStartButton()
+//     //     } else {
+//     //         console.log('no winner')
+//     //     }
+//     // })
+
+//     if (win == true) {
+//         gameWon()
+       
+//     } else {
+//         xTurn = !xTurn
+//     }
+// }
+
+function gameWon() {
         //display "WINNER"
         const winnerText = document.createElement('h1')
         winnerText.textContent = 'WINNER!'
+        //disable all other squares
+
         //Make winning xs or os Red
-        restartGame()
-    } else {
-        xTurn = !xTurn
-    }
+
+        //changeStartButton
+        resetStartButton()
+}
+
+function resetStartButton() {
+    startButton.textContent = 'Restart Game'
+    startButton.addEventListener('click', restartGame)
 }
 
 function restartGame() {
 {
-    startButton.textContent = 'Restart Game'
-    startButton.addEventListener('click', startGame)
+    // const allSquares = document.querySelectorAll('square')
+    // allSquares.remove()
+    const squaresToRemove = document.querySelectorAll('.square')
+    squaresToRemove.forEach(square => {
+        square.remove()
+    })
+
+    startGame() 
 }
 }
 
